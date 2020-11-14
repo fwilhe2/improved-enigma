7c676c36ae1ed1fc7af959330ce61270f4e07bb4
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-11-14 21:53:32 +0100

    test

--- common.main.kts
+++ common.main.kts
@@ -1,1 +1,7 @@
 fun exportVariable(name: String, value: String) = java.io.File(System.getenv("GITHUB_ENV")).appendText("${name}=${value}\n")
+
+fun withGroup(name: String, fn: () -> Unit) {
+    println("::group::${name}")
+    fn()
+    println("::endgroup::")
+}
